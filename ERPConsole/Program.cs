590fdb23fe16d6a70906d4debcb957b93f7d2921
sigmade <sychyov1991@mail.ru>
2020-11-30 16:24:14 +0300

    added Console App

---
+++ ERPConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using DBModels;
+
+namespace ERPConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MasterDBContext db = new MasterDBContext())
+            {
+                var users = db.People.ToList();
+                var result = from c in users where c.RegionId == "60" select c;
+                foreach (var u in result)
+                {
+                    Console.WriteLine($"User {u.FullName}");
+                }
+            }
+        }
+    }
+}
